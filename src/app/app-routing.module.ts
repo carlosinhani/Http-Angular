9c0162c09f255542ff146e68612cc02bf67f03fc
Carlos.inhani <carlos.inhani@gmail.com>
2021-01-22 09:09:42 -0300

    bootstrap_upload_form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '', pathMatch: 'full', redirectTo: 'cursos'
+    path: '', pathMatch: 'full', redirectTo: 'upload'
   },
   {
     path: 'cursos',
-    loadChildren:() => import('./cursos/cursos.module').then(m => m.CursosModule)
+    loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
+  },
+  {
+    path: 'upload',
+    loadChildren: () => import('./upload-file/upload-file.module').then(m => m.UploadFileModule)
   },
   {
     path: 'rxjs-poc',
     loadChildren: () => import('./unsubscribe-rxjs/unsubscribe-rxjs.module').then(m => m.UnsubscribeRxjsModule)
-  },
+  }
 ]
 
 @NgModule({
