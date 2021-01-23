791eb307c46717aea758a3ffa26e0acaf01cdcb5
Carlos.inhani <carlos.inhani@gmail.com>
2021-01-23 12:48:59 -0300

    event_http

--- server/src/index.js
+++ server/src/index.js
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // };
 // app.use(cors(corsOptions));
 
-const multipartMiddleware = multipart({ uploadDir: "./uploads" });
-app.post("/upload", multipartMiddleware, (req, res) => {
+const multipartMiddleware = multipart({ uploadDir: './uploads' });
+app.post('/upload', multipartMiddleware, (req, res) => {
   const files = req.files;
   console.log(files);
   res.json({ message: files });
@@ -23,5 +23,5 @@ app.post("/upload", multipartMiddleware, (req, res) => {
 app.use((err, req, res, next) => res.json({ error: err.message }));
 
 app.listen(8000, () => {
-  console.log("Servidor porta 8000");
+  console.log('Servidor porta 8000');
 });
--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 import { UploadFileService } from '../upload-file.service';
 
@@ -12,6 +13,7 @@ import { environment } from './../../../environments/environment.prod';
 export class UploadFileComponent implements OnInit {
 
   files: Set<File>;
+  progress = 0;
 
   constructor(
     private service: UploadFileService
@@ -32,12 +34,24 @@ export class UploadFileComponent implements OnInit {
       this.files.add(selectedFiles[i]);
     }
     document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+
+    this.progress = 0;
   }
 
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.service.upload(this.files, environment.BASE_URL + '/upload')
-        .subscribe(response => console.log('Upload Concluído'));
+        .subscribe((event: HttpEvent<Object>) => {
+          // HttpEventType
+          console.log(event);
+          if (event.type === HttpEventType.Response) {
+            console.log('Upload Concluído');
+          } else if (event.type === HttpEventType.UploadProgress) {
+            const percentDone = Math.round((event.loaded * 100) / event.total);
+            console.log('Progresso', percentDone);
+            this.progress = percentDone;
+          }
+        });
     }
   }
 }
