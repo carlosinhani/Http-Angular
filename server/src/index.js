22f20ff774cf840253d8896c220fac4afe7adb03
Carlos.inhani <carlos.inhani@gmail.com>
2021-01-24 07:37:48 -0300

    index_js

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const cors = require("cors");
 const bodyParser = require("body-parser");
+// const cors = require("cors");
 const multipart = require("connect-multiparty");
 
 const app = express();
